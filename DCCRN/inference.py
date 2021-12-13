924948602623adfaf8b1371bf4cc83b298676d8d
scott.meng <scott.meng@libratone.com.cn>
2021-12-13 20:26:32 +0800

    perf: update inference.py

--- DCCRN/inference.py
+++ DCCRN/inference.py
@@ -26,12 +26,15 @@ plt.switch_backend("agg")
 
 class Inferencer:
     def __init__(self, model, test_iter, config):
+        # set path
+        base_path = config["path"]["base"]
+        os.makedirs(base_path, exist_ok=True)
         # get checkpoints path
-        self.checkpoints_path = os.path.join(os.path.dirname(__file__), "checkpoints")
+        self.checkpoints_path = os.path.join(base_path, "checkpoints")
         # get output path
-        self.output_path = os.path.join(os.path.dirname(__file__), "enhanced")
+        self.output_path = os.path.join(base_path, "enhanced")
         # get logs path
-        self.logs_path = os.path.join(os.path.dirname(__file__), "logs", "inference")
+        self.logs_path = os.path.join(base_path, "logs", "inference")
         prepare_empty_path([self.output_path, self.logs_path])
 
         # set iter
@@ -49,6 +52,8 @@ class Inferencer:
         self.window = paddle.to_tensor(np.hanning(self.win_len), dtype=paddle.float32)
 
         # get inference args
+        self.n_folds = config["inference"]["n_folds"]
+        self.n_jobs = config["inference"]["n_jobs"]
         self.audio_visual_samples = config["inference"]["audio_visual_samples"]
 
         # config logs
@@ -80,10 +85,10 @@ class Inferencer:
             )
         self.writer_text_enh_clipped_step += 1
 
-    def audio_visualization(self, noisy, clean, enh, name):
-        self.writer.add_audio("audio/noisy", noisy, 1, sample_rate=self.sr)
-        self.writer.add_audio("audio/clean", clean, 1, sample_rate=self.sr)
-        self.writer.add_audio("audio/enh", enh, 1, sample_rate=self.sr)
+    def audio_visualization(self, noisy, clean, enh, name, epoch):
+        self.writer.add_audio(f"audio/noisy/{name}", noisy, epoch, sample_rate=self.sr)
+        self.writer.add_audio(f"audio/clean/{name}", clean, epoch, sample_rate=self.sr)
+        self.writer.add_audio(f"audio/enh/{name}", enh, epoch, sample_rate=self.sr)
 
         # Visualize the spectrogram of noisy speech, clean speech, and enhanced speech
         noisy_mag, _ = librosa.magphase(librosa.stft(noisy, n_fft=320, hop_length=160, win_length=320))
@@ -97,49 +102,90 @@ class Inferencer:
                 f"max: {np.max(mag):.3f}, "
                 f"min: {np.min(mag):.3f}"
             )
-            librosa.display.specshow(librosa.amplitude_to_db(mag), cmap="magma", y_axis="linear", ax=axes[k], sr=16000)
+            librosa.display.specshow(
+                librosa.amplitude_to_db(mag, ref=np.max), cmap="magma", y_axis="linear", ax=axes[k], sr=16000
+            )
         plt.tight_layout()
-        self.writer.add_figure(f"spec/{name}", fig, 1)
+        self.writer.add_figure(f"spec/{name}", fig, epoch)
 
-    def metrics_visualization(self, noisy_list, clean_list, enh_list, n_jobs=8):
-        noisy_stoi_score = Parallel(n_jobs=n_jobs)(
-            delayed(STOI)(noisy, clean) for noisy, clean in tqdm(zip(noisy_list, clean_list))
-        )
-        enh_stoi_score = Parallel(n_jobs=n_jobs)(
-            delayed(STOI)(noisy, clean) for noisy, clean in tqdm(zip(enh_list, clean_list))
-        )
-        noisy_stoi_score_mean = np.mean(noisy_stoi_score)
-        enh_stoi_score_mean = np.mean(enh_stoi_score)
-        self.writer.add_scalar("STOI/test/noisy", noisy_stoi_score_mean, 1)
-        self.writer.add_scalar("STOI/test/enh", enh_stoi_score_mean, 1)
+    def metrics_visualization(self, noisy_list, clean_list, enh_list, epoch, n_folds=1, n_jobs=8):
+        score = {
+            "noisy": {
+                "STOI": [],
+                "WB_PESQ": [],
+            },
+            "enh": {
+                "STOI": [],
+                "WB_PESQ": [],
+            },
+        }
 
-        noisy_wb_pesq_score = Parallel(n_jobs=n_jobs)(
-            delayed(WB_PESQ)(noisy, clean) for noisy, clean in tqdm(zip(noisy_list, clean_list))
-        )
-        enh_wb_pesq_score = Parallel(n_jobs=n_jobs)(
-            delayed(WB_PESQ)(noisy, clean) for noisy, clean in tqdm(zip(enh_list, clean_list))
-        )
-        noisy_wb_pesq_score_mean = np.mean(noisy_wb_pesq_score)
-        enh_wb_pesq_score_mean = np.mean(enh_wb_pesq_score)
-        self.writer.add_scalar("WB_PESQ/test/noisy", noisy_wb_pesq_score_mean, 1)
-        self.writer.add_scalar("WB_PESQ/test/enh", enh_wb_pesq_score_mean, 1)
+        split_num = len(noisy_list) // n_folds
+        for n in range(n_folds):
+            noisy_stoi_score = Parallel(n_jobs=n_jobs)(
+                delayed(STOI)(noisy, clean)
+                for noisy, clean in tqdm(
+                    zip(
+                        noisy_list[n * split_num : (n + 1) * split_num], clean_list[n * split_num : (n + 1) * split_num]
+                    )
+                )
+            )
+            enh_stoi_score = Parallel(n_jobs=n_jobs)(
+                delayed(STOI)(noisy, clean)
+                for noisy, clean in tqdm(
+                    zip(enh_list[n * split_num : (n + 1) * split_num], clean_list[n * split_num : (n + 1) * split_num])
+                )
+            )
+            score["noisy"]["STOI"].append(np.mean(noisy_stoi_score))
+            score["enh"]["STOI"].append(np.mean(enh_stoi_score))
 
-    def save_audio(self, audio_list, audio_files, n_jobs=8):
-        Parallel(n_jobs=n_jobs)(
-            delayed(sf.write)(audio_file, audio, samplerate=self.sr)
-            for audio_file, audio in tqdm(zip(audio_files, audio_list))
-        )
+            noisy_wb_pesq_score = Parallel(n_jobs=n_jobs)(
+                delayed(WB_PESQ)(noisy, clean)
+                for noisy, clean in tqdm(
+                    zip(
+                        noisy_list[n * split_num : (n + 1) * split_num], clean_list[n * split_num : (n + 1) * split_num]
+                    )
+                )
+            )
+            enh_wb_pesq_score = Parallel(n_jobs=n_jobs)(
+                delayed(WB_PESQ)(noisy, clean)
+                for noisy, clean in tqdm(
+                    zip(enh_list[n * split_num : (n + 1) * split_num], clean_list[n * split_num : (n + 1) * split_num])
+                )
+            )
+            score["noisy"]["WB_PESQ"].append(np.mean(noisy_wb_pesq_score))
+            score["enh"]["WB_PESQ"].append(np.mean(enh_wb_pesq_score))
+
+        self.writer.add_scalar("STOI/test/noisy", np.mean(score["noisy"]["STOI"]), epoch)
+        self.writer.add_scalar("STOI/test/enh", np.mean(score["enh"]["STOI"]), epoch)
+        self.writer.add_scalar("WB_PESQ/test/noisy", np.mean(score["noisy"]["WB_PESQ"]), epoch)
+        self.writer.add_scalar("WB_PESQ/test/enh", np.mean(score["enh"]["WB_PESQ"]), epoch)
+
+    def save_audio(self, audio_list, audio_files, n_folds=1, n_jobs=8):
+        split_num = len(audio_list) // n_folds
+        for n in range(n_folds):
+            Parallel(n_jobs=n_jobs)(
+                delayed(sf.write)(audio_file, audio, samplerate=self.sr)
+                for audio_file, audio in tqdm(
+                    zip(
+                        audio_files[n * split_num : (n + 1) * split_num],
+                        audio_list[n * split_num : (n + 1) * split_num],
+                    )
+                )
+            )
+
+    def set_model_to_eval_mode(self):
+        self.model.eval()
 
     def __call__(self):
-        self.model.eval()
-        audio_visual_samples_num = 0
+        self.set_model_to_eval_mode()
+
         noisy_list = []
         clean_list = []
+        noisy_files = []
         enh_list = []
         enh_files = []
         for noisy, clean, noisy_file in tqdm(self.test_iter, desc="test"):
-            assert len(noisy_file) == 1
-
             with paddle.no_grad():
                 enh = self.model(noisy)
 
@@ -148,23 +194,24 @@ class Inferencer:
             enh = enh.detach().squeeze(0).cpu().numpy()
             assert len(noisy) == len(clean) == len(enh)
 
-            audio_visual_samples_num += 1
-            if audio_visual_samples_num <= self.audio_visual_samples:
-                self.audio_visualization(noisy, clean, enh, noisy_file)
+            for i in range(len(noisy_file)):
+                enh_file = os.path.join(self.output_path, os.path.basename(noisy_file[i]).replace("noisy", "enh_noisy"))
+                self.check_clipped(enh[i], enh_file)
+                enh_files.append(enh_file)
 
-            enh_file = os.path.join(self.output_path, os.path.basename(noisy_file[0]).replace("noisy", "enh_noisy"))
+            noisy_list = np.concatenate([noisy_list, noisy], axis=0) if len(noisy_list) else noisy
+            clean_list = np.concatenate([clean_list, clean], axis=0) if len(clean_list) else clean
+            enh_list = np.concatenate([enh_list, enh], axis=0) if len(enh_list) else enh
+            noisy_files = np.concatenate([noisy_files, noisy_file], axis=0) if len(noisy_files) else noisy_file
 
-            noisy_list.append(noisy)
-            clean_list.append(clean)
-            enh_list.append(enh)
-            enh_files.append(enh_file)
+        # visual audio
+        for i in range(self.audio_visual_samples):
+            self.audio_visualization(noisy_list[i], clean_list[i], enh_list[i], os.path.basename(noisy_files[i]), 1)
 
-            self.check_clipped(enh, enh_file)
-
-        # visual metrics
-        self.metrics_visualization(noisy_list, clean_list, enh_list, n_jobs=8)
+        # visual metrics and get valid score
+        self.metrics_visualization(noisy_list, clean_list, enh_list, 1, n_folds=self.n_folds, n_jobs=self.n_jobs)
         # save audio
-        self.save_audio(enh_list, enh_files, n_jobs=8)
+        self.save_audio(enh_list, enh_files, n_folds=self.n_folds, n_jobs=self.n_jobs)
 
 
 if __name__ == "__main__":
@@ -181,6 +228,7 @@ if __name__ == "__main__":
     dataset_path = os.path.join(os.getcwd(), "dataset_csv")
 
     # get dataloader args
+    batch_size = config["dataloader"]["batch_size"]
     num_workers = 0 if device == "cpu" else config["dataloader"]["num_workers"]
     drop_last = config["dataloader"]["drop_last"]
 
@@ -188,7 +236,7 @@ if __name__ == "__main__":
     test_set = DNS_Dataset(dataset_path, config, mode="test")
     test_iter = DataLoader(
         test_set,
-        batch_size=1,
+        batch_size=batch_size[1],
         shuffle=False,
         num_workers=num_workers,
         drop_last=drop_last,
